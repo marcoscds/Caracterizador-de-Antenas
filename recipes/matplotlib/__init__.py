ab4f207d82490ff0d946b3a21b13dd4145c5155c
marcoscds <113212276+marcoscds@users.noreply.github.com>
2025-11-06 13:25:13 -0300

    recipe FINAL .py

--- recipes/matplotlib/__init__.py
+++ recipes/matplotlib/__init__.py
@@ -1,17 +1,16 @@
+import os
 from pythonforandroid.recipes.matplotlib import MatplotlibRecipe
 
 class FixedMatplotlibRecipe(MatplotlibRecipe):
-    # Corrige o empacotamento da libc++_shared.so
     need_stl_shared = True
 
-    # Força backend sem interface gráfica (necessário para Android)
     def get_recipe_env(self, arch=None, **kwargs):
         env = super().get_recipe_env(arch, **kwargs)
-        env["MPLBACKEND"] = "Agg"  # evita dependência em tkinter ou qt
-        env["MATPLOTLIBRC"] = "/data/data/org.kivy.matplotlib/matplotlibrc"
+        env["MPLBACKEND"] = "Agg"
+        env["MATPLOTLIBRC"] = os.path.join(os.getcwd(), "matplotlibrc")
+        env["USE_OPENMP"] = "0"  # evita erros de compilação com NDK modernos
         return env
 
-    # Nome interno da recipe (opcional, mas útil)
     name = "matplotlib"
 
 recipe = FixedMatplotlibRecipe()

ead043b069f76f2f891cb117cef4172e363e7e8e
marcoscds <113212276+marcoscds@users.noreply.github.com>
2025-11-07 03:50:49 -0300

    remove build_arch recipe.py

--- recipes/harfbuzz/__init__.py
+++ recipes/harfbuzz/__init__.py
@@ -1,16 +1,14 @@
 from pythonforandroid.recipe import Recipe
-from pythonforandroid.toolchain import shprint
-import sh
 
 class HarfbuzzPatchedRecipe(Recipe):
     version = "5.3.1"
     url = "https://github.com/harfbuzz/harfbuzz/archive/refs/tags/{version}.tar.gz"
 
-    def build_arch(self, arch):
-        env = self.get_recipe_env(arch)
+    def get_recipe_env(self, arch=None, **kwargs):
+        # Injeta as flags C/C++ ANTES do processo de build começar
+        env = super().get_recipe_env(arch, **kwargs)
         env["CFLAGS"] += " -Wno-error=cast-function-type-strict"
         env["CXXFLAGS"] += " -Wno-error=cast-function-type-strict"
-        shprint(sh.configure, "--with-freetype", _env=env)
-        shprint(sh.make, "-j4", _env=env)
-
+        return env
+        
 recipe = HarfbuzzPatchedRecipe()
